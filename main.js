0f1ed97c0acf64203eca5098ac2d2d8248054110
Mason Maron <mmaron101@gmail.com>
2025-12-17 21:09:30 -0600

    minor fixes

--- main.js
+++ main.js
@@ -25,19 +25,19 @@ const press = [
   {
     title: "Why a new type of bird is making its presence felt in the Pacific Northwest",
     url: "https://columbiainsight.org/why-a-new-type-of-bird-is-making-its-presence-felt-in-the-pacific-northwest/",
-    excerpt: "“Five, ten years ago there were no lesser goldfinches in the area at all,” says Maron. Today flocks of lesser goldfinches are common in many areas of the Columbia River Basin. A few decades ago, however, they were almost unknown in the Pacific Northwest outside of the Willamette Valley...",
+    excerpt: "'“Five, ten years ago there were no lesser goldfinches in the area at all,” says Maron. Today flocks of lesser goldfinches are common in many areas of the Columbia River Basin. A few decades ago, however, they were almost unknown in the Pacific Northwest outside of the Willamette Valley...'",
     date: "2025-07-03"
   },
   {
     title: "A Bigger Range for the Lesser Goldfinch",
     url: "https://www.allaboutbirds.org/news/bigger-range-lesser-goldfinch/",
-    excerpt: "Researchers from Washington State University and the Cornell Lab of Ornithology analyzed bird-observation data from two participatory-science initiatives from the Cornell Lab—Project FeederWatch and eBird—to track Lesser Goldfinch range changes over the past 25 years. The study, published in the journal Ornithology in April, shows that Lesser Goldfinch year-round populations more than doubled in size in Washington State, while also increasing substantially in Idaho and Oregon...",
+    excerpt: "'Researchers from Washington State University and the Cornell Lab of Ornithology analyzed bird-observation data from two participatory-science initiatives from the Cornell Lab—Project FeederWatch and eBird—to track Lesser Goldfinch range changes over the past 25 years. The study, published in the journal Ornithology in April, shows that Lesser Goldfinch year-round populations more than doubled in size in Washington State, while also increasing substantially in Idaho and Oregon...'",
     date: "2025-06-27"
   },
   {
     title: "A Bug’s Life Under Rough-legged Hawk Feathers",
     url: "http://wos.org/documents/wosnews/wosnews203.pdf",
-    excerpt: "I had set out with the assumption that more lice meant worse health for the birds, but I instead found a more complex relationship: younger hawks typically carried more lice than adults, and females of all ages typically carried more than males. There also seemed to be a relationship between lice load and health amongst the females, with birds carrying larger loads being in worse health, but not amongst the males...",
+    excerpt: "'I had set out with the assumption that more lice meant worse health for the birds, but I instead found a more complex relationship: younger hawks typically carried more lice than adults, and females of all ages typically carried more than males. There also seemed to be a relationship between lice load and health amongst the females, with birds carrying larger loads being in worse health, but not amongst the males...'",
     date: "2024-10-01"
   }
 ];
